547e8c41b829dfabb181e6ad5c1ab009497bc05c
VeilDalion <veil.marinas@umak.edu.ph>
2026-07-08 16:01:34 +0800

    Made transaction a composition of Account, changed attribute transactionType into integer.

--- src/Controllers/Account.java
+++ src/Controllers/Account.java
@@ -5,18 +5,21 @@ public class Account {
     private String name;
     private String accountNum;
     private int pin;
-    private double balance;
+    private static double balance;
+    Transaction transaction;
 
     Account(String name, String accountNum, int pin, double balance){
         this.name = name;
         this.accountNum = accountNum;
         this.pin = pin;
         this.balance = balance;
+
     }
     public double getBalance(){
         return this.balance;
     }
-    public double setBalance(double amount){
-
+    public double setBalance(int transactionType, double amount, String dateTime){
+        this.transaction = new Transaction(transactionType, amount, dateTime);
+        this.balance += amount;
     }
 }
--- src/Controllers/Transaction.java
+++ src/Controllers/Transaction.java
@@ -2,11 +2,11 @@ package Controllers;
 
 public class Transaction {
 
-    private String transactionType;
+    private int transactionType;
     private double amount;
     private String dateTime;
 
-    Transaction(String transactionType, double amount, String dateTime){
+    Transaction(int transactionType, double amount, String dateTime){
         this.transactionType = transactionType;
         this.amount = amount;
         this.dateTime = dateTime;
